60e92102f224fb3826c8b75e8f1d053dec628816
Joel Gustafson <joelg@mit.edu>
2017-10-12 19:46:50 -0400

    george michael

--- ipfs.js
+++ ipfs.js
@@ -6,19 +6,9 @@ function uuidV4() {
 
 const IPFS = {
     create() {
+        const options = {}
         return new Promise((resolve, reject) => {
-            const node = new Ipfs({
-                // config: {
-                //     Bootstrap: [
-                //         "/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
-                //         "/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3",
-                //         "/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
-                //         "/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
-                //         "/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
-                //         "/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64"
-                //     ]
-                // }
-            })
+            const node = new Ipfs(options)
             node.on("error", error => reject(error))
             node.on("ready", () => resolve(node))
         })
@@ -35,17 +25,41 @@ const IPFS = {
             })
         )
     },
+    tree(node, hash) {
+        const options = {}
+        return new Promise((resolve, reject) => {
+            node.dag.tree(hash, options, (error, value) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(value)
+                }
+            })
+        })
+    },
     put(node, tree) {
         const options = { format: "dag-cbor", hashAlg: "sha2-256" }
         // const options = {}
-        node.dag.put(tree, options, (error, cid) => {
-            console.log("cid", cid.toBaseEncodedString())
+        return new Promise((resolve, reject) => {
+            node.dag.put(tree, options, (error, cid) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(cid.toBaseEncodedString())
+                }
+            })
         })
     },
     get(node, hash) {
         const options = {}
-        node.dag.get(hash, options, (error, result) => {
-            console.log(result)
+        return new Promise((resolve, reject) => {
+            node.dag.get(hash, options, (error, result) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
         })
     },
     cat(node, hash) {
@@ -63,35 +77,24 @@ const IPFS = {
         )
     },
     async collect(node, hash) {
-        const map = {}
         const stream = await node.files.get(hash)
-        const files = await Promise.all(
-            await new Promise((resolve, reject) => {
-                const files = []
-                stream.on("error", error => reject(error))
-                stream.on("end", () => resolve(files))
-                stream.on("data", ({path, content}) => {
-                    if (content) {
-                        files.push(new Promise((resolve, reject) => {
-                            let text = ""
-                            content.on("error", error => reject(error))
-                            content.on("end", () => resolve({path, text}))
-                            content.on("data", data => text += data.toString())
-                            content.resume()
-                        }))
-                    } else {
-                        files.push({path})
-                    }
-                })
+        return new Promise((resolve, reject) => {
+            const files = []
+            stream.on("error", error => reject(error))
+            stream.on("end", () => resolve(Promise.all(files)))
+            stream.on("data", ({path, content, hash}) => {
+                if (content) {
+                    files.push(new Promise((resolve, reject) => {
+                        let text = ""
+                        content.on("error", error => reject(error))
+                        content.on("end", () => resolve({path, text}))
+                        content.on("data", data => text += data.toString())
+                        content.resume()
+                    }))
+                } else if (hash) {
+                    files.push({path, hash: node.types.multihash.toB58String(hash)})
+                }
             })
-        )
-
-        files.forEach(({path, text}) => {
-            const [hash, ...route] = path.split("/")
-            const name = route.pop()
-            const directory = route.reduce((parent, child) => parent[child], map)
-            directory[name] = text === undefined ? {} : text
         })
-        return map
     }
 }
